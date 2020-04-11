9b5d52bc1f57a6b8263ec1e8798ac6ffe9178f21
quanter <quanter.box@gmail.com>
2020-04-11 20:26:12 +0300

    Исправления

--- src/com/netcracker/lab/matrix/multyplyer/MultyplyerThread.java
+++ src/com/netcracker/lab/matrix/multyplyer/MultyplyerThread.java
@@ -1,7 +1,5 @@
 package com.netcracker.lab.matrix.multyplyer;
 
-// Источник: https://ru.stackoverflow.com/questions/473733/%D0%9A%D0%B0%D0%BA-%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D0%BB%D1%8C%D0%BD%D0%BE-%D1%83%D0%BC%D0%BD%D0%BE%D0%B6%D0%B8%D1%82%D1%8C-%D0%BC%D0%B0%D1%82%D1%80%D0%B8%D1%86%D1%83-%D0%BD%D0%B0-%D0%B4%D1%80%D1%83%D0%B3%D1%83%D1%8E-%D0%BC%D0%B0%D1%82%D1%80%D0%B8%D1%86%D1%83-%D0%B2-%D0%BD%D0%B5%D1%81%D0%BA%D0%BE%D0%BB%D1%8C%D0%BA%D0%B8%D1%85-%D0%BF%D0%BE%D1%82%D0%BE%D0%BA%D0%B0%D1%85
-
 /** Поток-вычислитель группы ячеек матрицы. */
 class MultiplierThread extends Thread
 {
@@ -48,8 +46,10 @@ class MultiplierThread extends Thread
     private void calcValue(final int row, final int col)
     {
         int sum = 0;
-        for (int i = 0; i < sumLength; ++i)
+        for (int i = 0; i < sumLength; ++i) {
             sum += firstMatrix[row][i] * secondMatrix[i][col];
+        }
+
         resultMatrix[row][col] = sum;
     }
 
@@ -62,8 +62,9 @@ class MultiplierThread extends Thread
                 + " по " + lastIndex + "...");
 
         final int colCount = secondMatrix[0].length;  // Число столбцов результирующей матрицы.
-        for (int index = firstIndex; index < lastIndex; ++index)
+        for (int index = firstIndex; index < lastIndex; ++index) {
             calcValue(index / colCount, index % colCount);
+        }
 
         System.out.println("Поток " + getName() + " завершил работу.");
     }
